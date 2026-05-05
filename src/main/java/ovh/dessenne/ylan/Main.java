35aa619b66e24faac68d777e2d62c5515a5f4c36
Torolgo <ylan.dessenne.spam@gmail.com>
2026-05-06 00:04:41 +0200

    feat(first-command) : add logic for all command with the first one

--- src/main/java/ovh/dessenne/ylan/Main.java
+++ src/main/java/ovh/dessenne/ylan/Main.java
@@ -1,17 +1,17 @@
 package ovh.dessenne.ylan;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import ovh.dessenne.ylan.listener.CommandListener;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+import static ovh.dessenne.ylan.BotConfig.TOKEN;
+
+public class Main {
+    static void main() throws Exception {
+        BotConfig.load();
+        JDA api = JDABuilder.createDefault(TOKEN).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
+        api.addEventListener(new CommandListener());
+        api.awaitShutdown();
     }
 }
---
+++ src/main/java/ovh/dessenne/ylan/command/ICommand.java
@@ -0,0 +1,11 @@
+package ovh.dessenne.ylan.command;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public interface ICommand {
+    String getName();
+
+    String getDescription();
+
+    void execute(MessageReceivedEvent event, String[] args);
+}
